3cd58a222e401a2b2f84beaef95a3e72551ff237
ryananjasmara <ryan.anjasmara@ralali.com>
2019-12-18 17:13:14 +0700

    Updates

---
+++ precache-manifest.5530c95e0ea439bcc3752af8374d88c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad4da56b26236a32470e18c9e5551ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bee0311d52f9349a5b4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e40adc50cc9fc208d3d6",
+    "url": "/static/js/2.9732c0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9732c0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bee0311d52f9349a5b4",
+    "url": "/static/js/main.c3694eeb.chunk.js"
+  },
+  {
+    "revision": "1ed6c913bded3e13228b",
+    "url": "/static/js/runtime-main.2d5db4c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
